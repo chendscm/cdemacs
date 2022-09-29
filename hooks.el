ffb893ad3e5da5748b0e1141ec4e0e14e5c32378
chendscm <chend.scm@gmail.com>
2022-09-29 08:10:55 +0800

    Update

--- hooks.el
+++ hooks.el
@@ -3,7 +3,7 @@
 (add-hook 'eshell-mode-hook 'cd-eshell-hook)
 (add-hook 'eww-mode-hook 'cd-eww-hook)
 (add-hook 'emacs-lisp-mode-hook 'cd-emacs-lisp-hook)
-;; (add-hook 'pdf-view-mode-hook 'cd-pdf-view-hook)
+(add-hook 'pdf-view-mode-hook 'cd-pdf-view-hook)
 (add-hook 'python-mode-hook 'cd-python-hook)
 (add-hook 'inferior-python-mode-hook 'cd-inferior-python-hook)
 (add-hook 'org-mode-hook 'cd-org-latex-impatient-hook)
--- modes/cd-eaf.el
+++ modes/cd-eaf.el
@@ -2,20 +2,22 @@
 
 ;; app
 (require 'eaf-browser)
-(require 'eaf-pdf-viewer)
 
 ;; browser
+(require 'eaf-pdf-viewer)
 (setq eaf-browser-continue-where-left-off t)
 (eaf-setq eaf-browser-enable-adblocker "true")
 (eaf-setq eaf-browser-remember-history "false")
 (setq eaf-browser-default-search-engine "duckduckgo")
 (eaf-setq eaf-browse-blank-page-url "https://duckduckgo.com")
 (eaf-setq eaf-browser-default-zoom "1.20")
-
 ;; Proxy
 (setq eaf-proxy-type "socks5")
 (setq eaf-proxy-host "127.0.0.1")
 (setq eaf-proxy-port "1080")
+
+;; terminal
+(require 'eaf-terminal)
 
 ;;
 
